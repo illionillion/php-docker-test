fef0160d352eee69796bc9a182ef890c3f9a64b4
yuto ichinose <yuto.0526.sano@gmail.com>
2023-04-19 01:11:50 +0900

    autocomplete追加

--- www/html/index.php
+++ www/html/index.php
@@ -13,13 +13,13 @@
     <h1>お問い合わせフォーム</h1>
     <form method="post" action="send-form.php">
         <label for="name">お名前</label>
-        <input type="text" id="name" name="name" required>
+        <input type="text" id="name" name="name" required autocomplete="off">
 
         <label for="email">メールアドレス</label>
-        <input type="text" id="email" name="email" required>
+        <input type="text" id="email" name="email" required autocomplete="off">
 
         <label for="subject">件名</label>
-        <input type="text" id="subject" name="subject" required>
+        <input type="text" id="subject" name="subject" required autocomplete="off">
 
         <label for="message">メッセージ</label>
         <textarea id="message" name="message" required></textarea>
