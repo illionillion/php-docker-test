6e5628dd164981b16ab2b729b4d422f52f0d61e5
yuto ichinose <yuto.0526.sano@gmail.com>
2023-04-23 02:13:25 +0900

    MySQLから一覧表示

--- www/html/inquiries.php
+++ www/html/inquiries.php
@@ -1,5 +1,11 @@
 <?php
+require_once 'db_connect.php';
 require_once 'dir-check.php';
+// SQL文を作成して実行する
+$sql = "SELECT * FROM inquiries";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$inquiries = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,6 +34,24 @@ require_once 'dir-check.php';
             </tr>
         <?php endwhile ?>
         <?php fclose($file); ?>
+    </table>
+    <table>
+        <tr>
+            <th>名前</th>
+            <th>メールアドレス</th>
+            <th>件名</th>
+            <th>メッセージ</th>
+            <th>時間</th>
+        </tr>
+        <?php foreach ($inquiries as $inquiry) : ?>
+            <tr>
+                <td><?= htmlspecialchars($inquiry['name']) ?></td>
+                <td><?= htmlspecialchars($inquiry['email']) ?></td>
+                <td><?= htmlspecialchars($inquiry['subject']) ?></td>
+                <td><?= htmlspecialchars($inquiry['message']) ?></td>
+                <td><?= htmlspecialchars($inquiry['created_at']) ?></td>
+            </tr>
+        <?php endforeach ?>
     </table>
 </body>
 
